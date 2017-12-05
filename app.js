0468de52303cdcb76c3a25f0151ab66ccc8ab220
jwink <jeff_winkler@yahoo.com>
2017-12-05 11:23:21 -0500

    with sse connections

--- app.js
+++ app.js
@@ -4,10 +4,56 @@ var app = express();
 
 global.path = require('path');
 
+var counter = 0;
+
+function sse(req, res, next) {
+  res.sseSetup = function() {
+    res.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive'
+    });
+  }
+
+  res.sseSend = function(data) {
+    res.write("data: " + data + "\n\n");
+  }
+
+  next();
+}
+
+var connections = [];
+
+app.use(sse);
+
 
 app.use('/', express.static(path.join(__dirname, 'public')), function(req, res, next) {
     next();
   });
+
+
+app.get('/ssetest', function(req, res) {
+  counter++;
+  for (var i = 0; i < connections.length; i++) {
+    connections[i].sseSend('hello world ' + counter + 'num connections: ' + connections.length);
+  }
+  res.sendStatus(200);
+});  
+
+app.get('/stream', function(req, res) {
+  counter++;
+  res.sseSetup();
+  res.sseSend('hello world ' + counter);
+  req.socket.on('close', function() {
+    console.log('connection closed');
+    console.log('index: ' + connections.indexOf(res));
+    var currIndex = connections.indexOf(res);
+    connections.splice(currIndex, 1);
+  });
+  connections.push(res);
+});
+
+
 
 var port = process.env.PORT || 1234;
 
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2,10 +2,27 @@
 
 
 $(document).ready(function() {
+    var source = new EventSource('/stream');
+    source.addEventListener('message', function(e) {
+        console.log(e.data);
+    });
+    source.addEventListener('open', function(e) {
+        console.log('connected');
+    });
+    source.addEventListener('error', function(e) {
+        console.log(e.target.readyState);
+    });
     console.log('whattup');
     $("#click-me-button").click(function(e) {
         //console.log(e);
         e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            url: '/ssetest',
+            success: function(data) {
+                console.log('response from GET: ' + data);
+            }
+        })
         console.log('button clicked!');
     });
 });
